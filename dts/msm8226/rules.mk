aa041aabe51ae136c95c96099d11032eab4d9be2
XRed_CubeX <xredcubex@gmail.com>
2021-06-09 23:00:52 +0200

    Add MatisseWifi Rev 3 and Specify my milletlte rev

--- dts/msm8226/rules.mk
+++ dts/msm8226/rules.mk
@@ -2,4 +2,5 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 DTBS += \
 	$(LOCAL_DIR)/apq8026-lge-lenok.dtb \
-	$(LOCAL_DIR)/msm8226-samsung-milletlte.dtb
+	$(LOCAL_DIR)/msm8226-samsung-rev4-milletlte.dtb\
+	$(LOCAL_DIR)/msm8226-samsung-rev3-matissewifi.dtb
